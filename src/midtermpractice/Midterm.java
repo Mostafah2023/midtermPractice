a57626bd15f35621541fd1a40f115cbf67287df3
Mostafa Hussain <hussamos@sheridancollege.ca>
2023-02-23 22:21:15 -0500

    First commit

---
+++ src/midtermpractice/Midterm.java
@@ -0,0 +1,19 @@
+package midtermpractice;
+import java.util.*;
+/**
+ *
+ * @author Mostafa Hussain
+ */
+public class Midterm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your grade: ");
+        int grade = scan.nextInt();
+        System.out.println("your grade is " + grade + "%");
+    }
+
+}
