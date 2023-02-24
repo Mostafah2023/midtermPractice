86d96de5e44375cae3c21ea316ffdf2189fb98a2
Mostafa Hussain <hussamos@sheridancollege.ca>
2023-02-23 22:26:49 -0500

    Revert "Second commit"
    This reverts commit edaa01f9e6fb69a40f5ff80f857157788f8cb800.

--- src/midtermpractice/Midterm.java
+++ src/midtermpractice/Midterm.java
@@ -11,16 +11,9 @@ public class Midterm {
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        //System.out.println("Enter your grade: ");
-        //int grade = scan.nextInt();
-        System.out.println("how much was the test out of?");
-        int total = scan.nextInt();
-        System.out.println("how much did you get?");
-        int mark = scan.nextInt();
-        
-        int grade = mark/total;
+        System.out.println("Enter your grade: ");
+        int grade = scan.nextInt();
         System.out.println("your grade is " + grade + "%");
-        
     }
 
 }
